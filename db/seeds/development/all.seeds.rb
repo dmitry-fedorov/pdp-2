0fc7ade09444d5ed6ee0e85bcd32cd2a2b40d4d0
dmitry-fedorov <dmitry.fedorov@flatstack.com>
2016-11-21 11:34:51 +0300

    add domain length limit

--- db/seeds/development/all.seeds.rb
+++ db/seeds/development/all.seeds.rb
@@ -3,5 +3,6 @@ FactoryGirl.create(:user, email: "user@example.com")
 3.times do
   owner = FactoryGirl.create(:user)
   company = FactoryGirl.create(:company, owner: owner, domain: "domain#{owner.id}")
+  owner.update(company: company)
   FactoryGirl.create_list(:user, 5, company: company)
 end
